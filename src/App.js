9910b2a9d912db5c7a0bc777481475b3057f7ebb
nehasangeetajha <nehasangeetajha@gmail.com>
2020-07-18 11:05:56 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent'
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 
@@ -12,6 +12,7 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
   );
 }
